cd712925e7d908faed44cd64a13821a74d6d9860
陆海空 <1031350293@qq.com>
2016-10-20 10:48:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/meituan/recyclerviewdemo/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.meituan.recyclerviewdemo;
+
+import android.content.Intent;
+import android.view.View;
+
+import com.example.meituan.recyclerviewdemo.recycler.custom.RecyclerViewCustomActivity;
+import com.example.meituan.recyclerviewdemo.recycler.gridlayout.RecyclerViewGridActivity;
+import com.example.meituan.recyclerviewdemo.recycler.linearlayout.RecyclerViewLinearActivity;
+import com.example.meituan.recyclerviewdemo.recycler.pinned.RecyclerViewPinnedActivity;
+import com.example.meituan.recyclerviewdemo.recycler.staggeredgrid.RecyclerViewStaggeredActivity;
+import com.wangjie.androidinject.annotation.annotations.base.AIClick;
+import com.wangjie.androidinject.annotation.annotations.base.AILayout;
+
+@AILayout(R.layout.activity_main)
+public class MainActivity extends BaseActivity {
+    @Override
+    @AIClick({R.id.recycler_view_main_linear_btn, R.id.recycler_view_main_grid_btn,
+            R.id.recycler_view_main_staggered_btn, R.id.recycler_view_main_custom_btn,
+            R.id.recycler_view_main_pinned_btn})
+    public void onClickCallbackSample(View view) {
+        switch (view.getId()) {
+            case R.id.recycler_view_main_linear_btn:
+                startActivity(new Intent(context, RecyclerViewLinearActivity.class));
+                break;
+            case R.id.recycler_view_main_grid_btn:
+                startActivity(new Intent(context, RecyclerViewGridActivity.class));
+                break;
+            case R.id.recycler_view_main_staggered_btn:
+                startActivity(new Intent(context, RecyclerViewStaggeredActivity.class));
+                break;
+            case R.id.recycler_view_main_custom_btn:
+                startActivity(new Intent(context, RecyclerViewCustomActivity.class));
+                break;
+            case R.id.recycler_view_main_pinned_btn:
+                startActivity(new Intent(context, RecyclerViewPinnedActivity.class));
+                break;
+        }
+    }
+}
